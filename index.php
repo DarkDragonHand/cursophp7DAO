835cf077caf99a57731978a65ec488901f49f6d1
Guilherme Potter Petry <guipotterrp@gmail.com>
2022-03-21 14:50:15 -0300

    Implementado metodos de lista

--- index.php
+++ index.php
@@ -8,9 +8,21 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
-$Joao = new Usuario();
+//Carrega um usuário
+//$Joao = new Usuario();
+//$Joao->LoadByID(2);
+//echo $Joao;
 
-$Joao->LoadByID(2);
-echo $Joao;
+//Carrega uma lista de usuários
+//$listaUsuarios = Usuario::getList();
+//echo json_encode($listaUsuarios);
+
+//$search = Usuario::search("Gui");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Gui", "123");
+echo $usuario;
 
 ?>
